9acafb7f4c2efc149ea7f05c9dd25f6c1929169e
robin <robinblue072@gmail.com>
2025-05-09 17:54:45 +1200

    removed theme experiment

--- layout.tsx
+++ layout.tsx
@@ -1,4 +1,3 @@
-import { Providers } from "./components/ThemeProvider";
 import Navbar from "./components/navbar";
 
 export default function RootLayout({
@@ -7,10 +6,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <Providers>{children}</Providers>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
